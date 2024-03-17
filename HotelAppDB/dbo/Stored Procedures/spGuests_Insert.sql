69c76742d50e7b73b730515291b312c3c3323273
ROSTOCHEK11 <rostzhol@gmail.com>
2024-03-17 11:31:01 +0100

    Added Booking method

---
+++ HotelAppDB/dbo/Stored Procedures/spGuests_Insert.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [dbo].[spGuests_Insert]
+	@firstName nvarchar(50), 
+	@lastName nvarchar(50)
+
+AS
+begin
+
+	set nocount on;
+
+	if not exists (select 1 from Guests where FirstName = @firstName and LastName = @lastName)
+	begin
+		insert into Guests (FirstName, LastName)
+		values (@firstName, @lastName);
+	end
+
+	select top 1 Id, FirstName, LastName
+	from Guests
+	where FirstName = @firstName and LastName = @lastName;
+	
+
+
+end
+
